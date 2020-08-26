d2348a0cb86511f1e3c53e654854611881065cf7
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2020-08-26 16:18:25 -0600

    Bundled w/m choice for survey options

--- code/web/src/modules/survey/SurveyPage.js
+++ code/web/src/modules/survey/SurveyPage.js
@@ -24,7 +24,7 @@ class SurveyPage extends PureComponent {
     super(props)
     this.state = {
 			isLoading: false,
-			parsedItems: []
+			parsedItems: {rocker: [], bohemian: [], business: [], artsy: []}
     }
 	}
 	
@@ -37,8 +37,13 @@ class SurveyPage extends PureComponent {
 	}
 	
 	componentDidUpdate() {
-		if (this.state.parsedItems.length === 0) {
-			const filteredItems = this.props.surveyProducts.products.filter(item => item.isSurvey)
+		if (this.state.parsedItems.rocker.length === 0) {
+			const inSurveyItems = this.props.surveyProducts.products.filter(item => item.isSurvey);
+			const filteredItems = inSurveyItems.reduce((acc, item) => {
+				acc[item.style].push(item);
+				
+				return acc;
+			}, {rocker: [], bohemian: [], business: [], artsy: []}); 
 			this.setState({parsedItems: filteredItems})
 		}
 	}
@@ -58,24 +63,137 @@ class SurveyPage extends PureComponent {
           </GridCell>
         </Grid>
 
-			  {/* Make 3 sections, map over each one to make the card */}
 				<Grid>
-					<H4 font="secondary" style={{ marginLeft: '1em' }}>Tops</H4>
+					<H4 font="secondary" style={{ marginLeft: '.2em', marginTop: '1em' }}>Tops</H4>
 				</Grid>
-				<Grid style={{ marginLeft: '1em' }}>
-					{this.state.parsedItems.map((item, i) => {
-						return (
-							<GridCell>
-							<Card style={{ marginTop: '2em', width: '12em', backgroundColor: white }} key={i}>
-								<img src={routeImage + item.image} alt={item.name} style={{ width: '100%' }}/>
-								<input type="radio" />
+				{this.state.parsedItems.rocker.length > 0 ? 
+					(<Grid style={{ height: '24vh', width: '100vw', display: 'flex', flexWrap: 'nowrap', overflowX: 'auto' }}>
+						<GridCell>
+							<Card style={{ marginTop: '2em', width: '19em', backgroundColor: white, marginLeft: '9px' }} key='rocker-top'>
+								<div style={{ display: 'flex' }}>
+									<img src={routeImage + this.state.parsedItems.rocker[1].image} alt={this.state.parsedItems.rocker[1].name} style={{ width: '50%' }}/>
+									<img src={routeImage + this.state.parsedItems.rocker[2].image} alt={this.state.parsedItems.rocker[2].name} style={{ width: '50%' }}/>
+								</div>
+								<input type="radio" style={{ marginLeft: '50%' }} />
 							</Card>
-							</GridCell>
-							)
-						})
-					}
+						</GridCell>
+						<GridCell>
+							<Card style={{ marginTop: '2em', width: '19em', backgroundColor: white, marginLeft: '9px' }} key='bohemian-top'>
+								<div style={{ display: 'flex' }}>
+									<img src={routeImage + this.state.parsedItems.bohemian[1].image} alt={this.state.parsedItems.bohemian[1].name} style={{ width: '50%' }}/>
+									<img src={routeImage + this.state.parsedItems.bohemian[2].image} alt={this.state.parsedItems.bohemian[2].name} style={{ width: '50%' }}/>
+								</div>
+								<input type="radio" style={{ marginLeft: '50%' }} />
+							</Card>
+						</GridCell>
+						<GridCell>
+							<Card style={{ marginTop: '2em', width: '19em', backgroundColor: white, marginLeft: '9px' }} key='business-top'>
+								<div style={{ display: 'flex' }}>
+									<img src={routeImage + this.state.parsedItems.business[1].image} alt={this.state.parsedItems.business[1].name} style={{ width: '50%' }}/>
+									<img src={routeImage + this.state.parsedItems.business[2].image} alt={this.state.parsedItems.business[2].name} style={{ width: '50%' }}/>
+								</div>
+								<input type="radio" style={{ marginLeft: '50%' }} />
+							</Card>
+						</GridCell>
+						<GridCell>
+							<Card style={{ marginTop: '2em', width: '19em', backgroundColor: white, marginLeft: '9px' }} key='artsy-top'>
+								<div style={{ display: 'flex' }}>
+									<img src={routeImage + this.state.parsedItems.artsy[1].image} alt={this.state.parsedItems.artsy[1].name} style={{ width: '50%' }}/>
+									<img src={routeImage + this.state.parsedItems.artsy[2].image} alt={this.state.parsedItems.artsy[2].name} style={{ width: '50%' }}/>
+								</div>
+								<input type="radio" style={{ marginLeft: '50%' }} />
+							</Card>
+						</GridCell>
+					</Grid>) : null
+				}
+
+				<Grid>
+					<H4 font="secondary" style={{ marginLeft: '.2em', marginTop: '1em' }}>Bottoms</H4>
 				</Grid>
-				
+				{this.state.parsedItems.rocker.length > 0 ? 
+					(<Grid style={{ height: '24vh', width: '100vw', display: 'flex', flexWrap: 'nowrap', overflowX: 'auto' }}>
+						<GridCell>
+							<Card style={{ marginTop: '2em', width: '19em', backgroundColor: white, marginLeft: '9px' }} key='rocker-bottom'>
+								<div style={{ display: 'flex' }}>
+									<img src={routeImage + this.state.parsedItems.rocker[3].image} alt={this.state.parsedItems.rocker[3].name} style={{ width: '50%' }}/>
+									<img src={routeImage + this.state.parsedItems.rocker[4].image} alt={this.state.parsedItems.rocker[4].name} style={{ width: '50%' }}/>
+								</div>
+								<input type="radio" style={{ marginLeft: '50%' }} />
+							</Card>
+						</GridCell>
+						<GridCell>
+							<Card style={{ marginTop: '2em', width: '19em', backgroundColor: white, marginLeft: '9px' }} key='bohemian-bottom'>
+								<div style={{ display: 'flex' }}>
+									<img src={routeImage + this.state.parsedItems.bohemian[3].image} alt={this.state.parsedItems.bohemian[3].name} style={{ width: '50%' }}/>
+									<img src={routeImage + this.state.parsedItems.bohemian[4].image} alt={this.state.parsedItems.bohemian[4].name} style={{ width: '50%' }}/>
+								</div>
+								<input type="radio" style={{ marginLeft: '50%' }} />
+							</Card>
+						</GridCell>
+						<GridCell>
+							<Card style={{ marginTop: '2em', width: '19em', backgroundColor: white, marginLeft: '9px' }} key='business-bottom'>
+								<div style={{ display: 'flex' }}>
+									<img src={routeImage + this.state.parsedItems.business[3].image} alt={this.state.parsedItems.business[3].name} style={{ width: '50%' }}/>
+									<img src={routeImage + this.state.parsedItems.business[4].image} alt={this.state.parsedItems.business[4].name} style={{ width: '50%' }}/>
+								</div>
+								<input type="radio" style={{ marginLeft: '50%' }} />
+							</Card>
+						</GridCell>
+						<GridCell>
+							<Card style={{ marginTop: '2em', width: '19em', backgroundColor: white, marginLeft: '9px' }} key='artsy-bottom'>
+								<div style={{ display: 'flex' }}>
+									<img src={routeImage + this.state.parsedItems.artsy[3].image} alt={this.state.parsedItems.artsy[3].name} style={{ width: '50%' }}/>
+									<img src={routeImage + this.state.parsedItems.artsy[4].image} alt={this.state.parsedItems.artsy[4].name} style={{ width: '50%' }}/>
+								</div>
+								<input type="radio" style={{ marginLeft: '50%' }} />
+							</Card>
+						</GridCell>
+					</Grid>) : null
+				}
+				<Grid>
+					<H4 font="secondary" style={{ marginLeft: '.2em', marginTop: '1em' }}>Shoes</H4>
+				</Grid>
+				{this.state.parsedItems.rocker.length > 0 ? 
+					(<Grid style={{ height: '24vh', width: '100vw', display: 'flex', flexWrap: 'nowrap', overflowX: 'auto' }}>
+						<GridCell>
+							<Card style={{ marginTop: '2em', width: '19em', backgroundColor: white, marginLeft: '9px' }} key='rocker-shoes'>
+								<div style={{ display: 'flex' }}>
+									<img src={routeImage + this.state.parsedItems.rocker[5].image} alt={this.state.parsedItems.rocker[5].name} style={{ width: '50%' }}/>
+									<img src={routeImage + this.state.parsedItems.rocker[6].image} alt={this.state.parsedItems.rocker[6].name} style={{ width: '50%' }}/>
+								</div>
+								<input type="radio" style={{ marginLeft: '50%' }} />
+							</Card>
+						</GridCell>
+						<GridCell>
+							<Card style={{ marginTop: '2em', width: '19em', backgroundColor: white, marginLeft: '9px' }} key='bohemian-shoes'>
+								<div style={{ display: 'flex' }}>
+									<img src={routeImage + this.state.parsedItems.bohemian[5].image} alt={this.state.parsedItems.bohemian[5].name} style={{ width: '50%' }}/>
+									<img src={routeImage + this.state.parsedItems.bohemian[6].image} alt={this.state.parsedItems.bohemian[6].name} style={{ width: '50%' }}/>
+								</div>
+								<input type="radio" style={{ marginLeft: '50%' }} />
+							</Card>
+						</GridCell>
+						<GridCell>
+							<Card style={{ marginTop: '2em', width: '19em', backgroundColor: white, marginLeft: '9px' }} key='business-shoes'>
+								<div style={{ display: 'flex' }}>
+									<img src={routeImage + this.state.parsedItems.business[5].image} alt={this.state.parsedItems.business[5].name} style={{ width: '50%' }}/>
+									<img src={routeImage + this.state.parsedItems.business[6].image} alt={this.state.parsedItems.business[6].name} style={{ width: '50%' }}/>
+								</div>
+								<input type="radio" style={{ marginLeft: '50%' }} />
+							</Card>
+						</GridCell>
+						<GridCell>
+							<Card style={{ marginTop: '2em', width: '19em', backgroundColor: white, marginLeft: '9px' }} key='artsy-shoes'>
+								<div style={{ display: 'flex' }}>
+									<img src={routeImage + this.state.parsedItems.artsy[5].image} alt={this.state.parsedItems.artsy[5].name} style={{ width: '50%' }}/>
+									<img src={routeImage + this.state.parsedItems.artsy[6].image} alt={this.state.parsedItems.artsy[6].name} style={{ width: '50%' }}/>
+								</div>
+								<input type="radio" style={{ marginLeft: '50%' }} />
+							</Card>
+						</GridCell>
+					</Grid>) : null
+				}
+
 				<Grid>
 				  <Button theme="secondary" style={{ marginLeft: '50%', marginTop: '2em', marginBottom: '2em' }}>Submit</Button>
 				</Grid>

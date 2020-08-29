1c7986436c80ef3a319e7064442f8a3bec309ff2
Joe Haefling <josephhaefling@gmail.com>
2020-08-29 13:16:13 -0600

    Add store

--- code/web/src/modules/survey/survey.test.js
+++ code/web/src/modules/survey/survey.test.js
@@ -1,21 +1,95 @@
 import React from 'react'
 import SurveyPage from './SurveyPage'
-import { render } from '@testing-library/react'
+import { render, waitFor } from '@testing-library/react';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import { MemoryRouter } from 'react-router-dom'
+import configureMockStore from 'redux-mock-store';
+import '@testing-library/jest-dom';
+import * as types from './api/actions'
+import fetchMock from 'fetch-mock'
+import { query, mutation } from 'gql-query-builder'
+
+
+// import user from '../../setup/routes/user';
+jest.mock('./api/actions')
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const mockServiceCreator = (body, succeeds = true) => () =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => (succeeds ? resolve(body) : reject(body)), 10);
+    });
+
+    describe('actions', () => {
+            it.only('should create an action to add a todo', () => {
+                fetchMock.getOnce('/surveyProducts', query({
+                        operation: 'products',
+                        fields: ['name', 'description', 'image', 'style', 'isSurvey', 'category']
+                })
+                )
+
+                const expectedActions = [
+                    {
+                        type: types.GET_SURVEY_PRODUCTS, isLoading: false,
+                        error: null,
+                        list:'thing' },
+                    {
+                        type: types.SURVEY_GET_LIST_FAILURE, 
+                        error: 'Some error occurred. Please try again.',
+                        isLoading: false 
+                    }
+                ]
+
+                const store = mockStore({ surveyItems: [] })
+
+                return store.dispatch(types.getSurveyProducts()).then(() => {
+                    // return of async actions
+                    expect(store.getActions()).toEqual(expectedActions)
+                })
+            })
+    })  
 
 describe('SurveyPage', () => {
 
     let surveyPageElement
-    const surveyState = {
-        user: 3,
-        surveyProducts:  [{description: 'Product1'}]
+    
+    // const getSurveyProducts = jest.fn()
+    // const parseSurveyItems = jest.fn()
+    // const surveyState = jest.fn(state => {})
+
+    const state = {
+        user: {
+            details: 'Joe',
+            error: null,
+            isAuthenticated: true,
+            isLoading: false
+        },
+        products: {
+            error: '',
+            isLoading: false,
+            List: []
+        }
     }
-    const getSurveyProducts = jest.fn()
-    const parseSurveyItems = jest.fn()
+
+    // const store = {
+    //     subscribe: () => {},
+    //     getState: () => {},
+    //     dispatch: ()=> {}
+    // }
+    let store
 
     beforeEach(() => {
+
+        store = mockStore(
+            {
+                subscribe: () => { },
+                getState: () => { },
+                dispatch: () => { }
+            }
+        );
+
         surveyPageElement = (
-            <Provider>
+            <Provider store={store}>
                 <MemoryRouter>
                     <SurveyPage />
                 </MemoryRouter>
@@ -23,11 +97,15 @@ describe('SurveyPage', () => {
     )
     })
 
-    it('Should render the survey page', () => {
+    it('Should render the survey page', async () => {
         const { getByTestId, getByText } = render(surveyPageElement)
-        const header = getByTestId('stylePage-greeting')
-
-        expect(header).toBeInTheDocument()
+        
+        await waitFor(() => {
+            const header = getByTestId('stylePage-greeting') 
+            
+            expect(header).toBeInTheDocument()
+        })
+        
     }) 
         
 })
